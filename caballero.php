3f4f0a8b967a9c4ad7c65b0b40ece5b3cf7976c1
alumno115gcb16 <leticia110497@gmail.com>
2014-12-09 22:38:35 -0600

    sitio
    version1

---
+++ caballero.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>caballero</title>
+<link href="jquery-mobile/tema.min.css" rel="stylesheet" type="text/css"/>
+<link href="jquery-mobile/jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
+<script src="jquery-mobile/jquery-1.6.4.min.js" type="text/javascript"></script>
+<script src="jquery-mobile/jquery.mobile-1.0.min.js" type="text/javascript"></script>
+<script src="phonegap.js" ></script>
+<style type="text/css">
+body,td,th {
+	color: #0F0;
+}
+body {
+	background-color: #09F;
+}
+</style>
+
+</head>
+
+<body>
+<div data-role="page" id="Principal" data-theme="a">
+   <div data-role="header" data-theme="a">
+   <h1>Tenis para CABALLERO </h1>
+   
+</div>
+
+<div data-role="content" data-theme="a" align="center">
+<div data-role="collapsible">
+<h2>Tipos de Tenis </h2>
+<div>
+
+La empresa de Tenis Pirma Brasil hace referencia con sus tenis y modelos para hombres deportistas asi como a hombres que solo buscan comodidad en sus pies y lucir bien.
+  <br>
+  </ul>
+</div>
+   
+   </div>
+   
+</div>
+
+<div>
+               <ul data-role="listview">
+               <li><a href="#PaginaDE">Deportivos</a></li>
+               <li><a href="#PaginaOR">Ordinarios</a></li>
+               </ul>
+</div>
+<div data-role="footer" align="center" data-theme="a">
+<div><img src="imagenes/footer.jpg" width="78" height="44" data-position="fixed"></div>
+<div>
+     <h4>Leticia Luna Meyo</h4>
+     </div>
+     </div>
+</div>
+
+<div data-role="page" id="PaginaDE" data-add-back-btn="true" data-theme="a">
+     <div data-role="header" data-theme="a">
+        <h1>Deportivos</h1>
+        
+         </div>
+         <div data-role="content" align="center" data-theme="a">
+       <div>
+<!-- Start WOWSlider.com -->
+<iframe src="caballero.html" style="width:1026px;height:396px;max-width:100%;overflow:hidden;border:none;padding:0;margin:0 auto;display:block;" marginheight="0" marginwidth="0"></iframe>
+<!-- End WOWSlider.com --></div>
+</div>
+      <div data-role="footer" data-theme="a" data-position="fixed">
+             <h4>Leticia Luna Meyo</h4>
+             
+</div>
+</div>
+
+<div data-role="page" id="PaginaOR" data-add-back-btn="true" data-theme="a">
+     <div data-role="header" data-theme="a">
+        <h1>Ordinarios</h1>
+        
+         </div>
+         <div data-role="content" align="center" data-theme="a">
+       <div>
+<!-- Start WOWSlider.com -->
+<iframe src="caballero2.html" style="width:978px;height:402px;max-width:100%;overflow:hidden;border:none;padding:0;margin:0 auto;display:block;" marginheight="0" marginwidth="0"></iframe>
+<!-- End WOWSlider.com --></div>
+</div>
+      <div data-role="footer" data-theme="a" data-position="fixed">
+             <h4>Leticia Luna Meyo</h4>
+             
+</div>
+</div>
+
+</body>
+</html>
---
+++ dama.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>dama</title>
+<link href="jquery-mobile/tema.min.css" rel="stylesheet" type="text/css"/>
+<link href="jquery-mobile/jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
+<script src="jquery-mobile/jquery-1.6.4.min.js" type="text/javascript"></script>
+<script src="jquery-mobile/jquery.mobile-1.0.min.js" type="text/javascript"></script>
+<script src="phonegap.js" ></script>
+<style type="text/css">
+body,td,th {
+	color: #FFF;
+}
+body {
+	background-color: #C6F;
+}
+.ss {
+	font-family: Times New Roman, Times, serif;
+}
+</style>
+</head>
+<h3 align="right"><a href="index.php#index">HOME</A></h3>
+<body>
+
+<div data-role="page" id="Principal" data-theme="a">
+   <div data-role="header" data-theme="a">
+   <h1>Tenis para DAMA</h1>
+   
+</div>
+<div data-role="content" data-theme="a" align="center">
+<div data-role="collapsible">
+<h2>Tipos de Tenis </h2>
+<div>
+
+La empresa de Tenis Pirma Brasil hace referencia con sus tenis y modelos tanto a mujeres deportistas como a mujeres  que solo buscan comodidad en sus pies y lucir bien.
+  <br>
+  </ul>
+</div>
+   
+   </div>
+   
+</div>
+
+<div>
+               <ul data-role="listview">
+               <li><a href="#PaginaDE">Deportivos</a></li>
+               <li><a href="#PaginaOR">Ordinarios</a></li>
+               </ul>
+</div>
+<div data-role="footer" align="center" data-theme="a">
+<div><img src="imagenes/footer.jpg" width="78" height="44"></div>
+<div>
+     <h4>Leticia Luna Meyo</h4>
+     </div>
+     </div>
+</div>
+
+<div data-role="page" id="PaginaDE" data-add-back-btn="true" data-theme="a">
+     <div data-role="header" data-theme="a">
+        <h1>Deportivos</h1>
+        
+         </div>
+         <div data-role="content" align="center" data-theme="a">
+       <div>
+<!-- Start WOWSlider.com -->
+<iframe src="dama.html" style="width:960px;height:360px;max-width:100%;overflow:hidden;border:none;padding:0;margin:0 auto;display:block;" marginheight="0" marginwidth="0"></iframe>
+<!-- End WOWSlider.com --></div>
+</div>
+      <div data-role="footer" data-theme="a" data-position="fixed">
+             <h4>Leticia Luna Meyo</h4>
+             
+</div>
+</div>
+
+<div data-role="page" id="PaginaOR" data-add-back-btn="true" data-theme="a">
+     <div data-role="header" data-theme="a">
+        <h1>Ordinarios</h1>
+        
+         </div>
+         <div data-role="content" align="center" data-theme="a">
+       <div>
+<!-- Start WOWSlider.com -->
+<iframe src="dama2.html" style="width:974px;height:360px;max-width:100%;overflow:hidden;border:none;padding:0;margin:0 auto;display:block;" marginheight="0" marginwidth="0"></iframe>
+<!-- End WOWSlider.com --></div>
+</div>
+      <div data-role="footer" data-theme="a" data-position="fixed">
+             <h4>Leticia Luna Meyo</h4>
+             
+</div>
+</div>
+
+</body>
+</html>
